9c3d2322974afdc5239090e2f929f700f5bfe737
kotaro <ktr.0702.ot@gmail.com>
2026-06-05 15:26:17 +0900

    Step7: Terraform+AnsibleでフルIaC構成

---
+++ terraform-ansible/terraform/main.tf
@@ -0,0 +1,105 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+# サブネット
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "${var.project_name}-subnet"
+  }
+}
+
+# インターネットゲートウェイ
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+# ルートテーブル
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id  = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "${var.project_name}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id       = aws_subnet.main.id
+  route_table_id  = aws_route_table.main.id
+}
+
+# セキュリティグループ
+resource "aws_security_group" "main" {
+  name    = "${var.project_name}-sg"
+  vpc_id  = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+
+# キーペア
+resource "aws_key_pair" "main" {
+  key_name    = "${var.project_name}-key"
+  public_key = var.public_key
+}
+
+# EC2
+resource "aws_instance" "main" {
+  ami                           = var.ami
+  instance_type                 = var.instance_type
+  subnet_id                     = aws_subnet.main.id
+  vpc_security_group_ids        = [aws_security_group.main.id]
+  key_name                      = aws_key_pair.main.key_name
+  associate_public_ip_address   = true
+  
+  tags = {
+    Name = "${var.project_name}-ec2"
+  }
+}
---
+++ terraform-ansible/terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "region" {
+  description = "AWSリージョン"
+  default     = "ap-northeast-1"
+}
+
+variable "project_name" {
+  description = "プロジェクト名"
+  default     = "terraform-ansible"
+}
+
+variable "vpc_cidr" {
+  description = "VPCのCIDRブロック"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "サブネットのCIDRブロック"
+  default     = "10.0.1.0/24"
+}
+
+variable "ami" {
+  description = "AMI ID"
+  default     = "ami-0599b6e53ca798bb2"
+}
+
+variable "instance_type" {
+  description = "インスタンスタイプ"
+  default     = "t2.micro"
+}
+
+variable "public_key" {
+  description = "SSH公開鍵"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+zSni65ny0keoMCgdlSdYJ41piQuCzHAgd1Lj+t+P2w5UcDu+yxwulRLWVsARmBj3HgKhqlP2WSoC0SbvdjiyY7XdV1StjIIVNYwLhWpiURFgmmUMvbtsRNHw82XUyrxg4Tj1sdDzDUk3Bx2KQbYmstUtz7VkfwHpTodrLxRio32Mj0xZgJ+GcnKqc7i8hG+YSI15MHP+S9vToOdBbNiyoEDRoA02ktq3dTOPbNHhI+DVMtrKnisqVWE+fgAEtxwcYPIHgqP5bminFfl79MlNRtJFf/NitHBGYmRf3CqGuDnzV4WhqSjSoCneHlPVnHSBO9z8jsJ8UEpWaQ4OhWstnuj0NNE10NcjdZJ8l1AKiiPPxaxt5UINGaREpXstfvzRtSzqJbsxGpfxpPyGUVDjMWee9371bxrxdBhFH1fD+cBLuutjjlzyNtJkAMuGrOoZFYdqs8eZTrpkINzPho26o7ddJxzqibFn0om1YWY0SWI2UZNiwkOqT7O2iN+9F4ez6bGyGUv2y8AVnLgiMj5HKWC3lQijtLzn/9cObSvKA4aBt6aPLQ7oREYV//zXIxCi39WVPuTE4bPdDnb6EfSylMDBuY08mn3F6M1dW0g8Uc4DalhUcZFnFiVQhIbwiXYeIcTZUNNJdNumNwOu7EVUZEUnjtPUp7cRL7hM6IePuQ=="
+}
